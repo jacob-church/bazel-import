6419a9f3c93833d96bf978448a764cf63b4d13c3
Jacob Church <jchurch@lucidchart.com>
2024-09-16 12:45:52 -0600

    move bazel dep method to separate script

---
+++ src/bazeltools.ts
@@ -0,0 +1,27 @@
+import * as vscode from 'vscode';
+import {uriToContainingUri} from './uritools';
+import {uriToBuildTarget} from './targettools';
+/**
+ * Takes a list of file Uris and returns the corresponding build targets
+ * @param uris
+ * @param currentTarget
+ * @returns
+ */
+export async function getImportedTargets(uris: vscode.Uri[], currentTarget: string): Promise<Set<string>> {
+    const depPromises = await Promise.all(
+        uris.map((uri) => {
+            const dir = uriToContainingUri(uri);
+            return uriToBuildTarget(dir);
+        }),
+    );
+    const depTargets = new Set<string>();
+    for (const promise of depPromises) {
+        if (promise) {
+            const [target, _] = promise;
+            if (target !== currentTarget) {
+                depTargets.add(target);
+            }
+        }
+    }
+    return depTargets;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import {uriToContainingUri} from './uritools';
 import {uriToBuildTarget} from './targettools';
 import {positionsFromTextChanges, urisFromTextChanges} from './importparse';
+import {getImportedTargets} from './bazeltools';
 
 const OPEN_BUTTON = 'Open';
 const DISMISS_BUTTON = "Don't show this again";
@@ -68,31 +69,6 @@ export function activate(context: vscode.ExtensionContext) {
                 });
         }
     });
-}
-
-/**
- * Takes a list of file Uris and returns the corresponding build targets
- * @param uris
- * @param currentTarget
- * @returns
- */
-export async function getImportedTargets(uris: vscode.Uri[], currentTarget: string): Promise<Set<string>> {
-    const depPromises = await Promise.all(
-        uris.map((uri) => {
-            const dir = uriToContainingUri(uri);
-            return uriToBuildTarget(dir);
-        }),
-    );
-    const depTargets = new Set<string>();
-    for (const promise of depPromises) {
-        if (promise) {
-            const [target, _] = promise;
-            if (target !== currentTarget) {
-                depTargets.add(target);
-            }
-        }
-    }
-    return depTargets;
 }
 
 export function deactivate() {
