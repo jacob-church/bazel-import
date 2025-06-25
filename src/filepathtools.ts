0cc469fa1a837c71c3578287cd1c8ec20488df63
Charles Johnson <charlesjohnson@lucidchart.com>
2025-06-25 13:40:48 -0600

    Get path replacements from tsconfig

---
+++ src/filepathtools.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode'; 
+import * as ts from 'typescript';
+import path = require('path');
+import { filePathToTargetPath } from './targettools';
+
+const tsExtension = vscode.extensions.getExtension('vscode.typescript-language-features');
+let active = true; 
+    
+if (!tsExtension) {
+    vscode.window.showErrorMessage('TypeScript Language Features extension is not enabled.');
+    active = false; 
+}
+
+export function resolveModuleToUri(
+    hostFileUri: vscode.Uri,
+    moduleSpecifier: string
+) {
+    const configPath = ts.findConfigFile(hostFileUri.fsPath, ts.sys.fileExists, 'tsconfig.json'); // COuld we use this to get the bazel build?
+
+    if (!configPath) {
+        return undefined; 
+    }
+
+    const configContent = ts.sys.readFile(configPath);
+    if (configContent) {
+        const { config, error } = ts.parseConfigFileTextToJson(configPath, configContent);
+        if (error) {
+            console.error(`Error parsing tsconfig file ${configPath}:`);
+            return; 
+        }
+            
+        const parsedCommandLine = ts.parseJsonConfigFileContent(
+                config,
+                ts.sys,
+                path.dirname(configPath)
+            );
+        console.log(parsedCommandLine); 
+
+        const moduleResolutionHost: ts.ModuleResolutionHost = {
+            fileExists: ts.sys.fileExists,
+            readFile: ts.sys.readFile,
+            directoryExists: ts.sys.directoryExists,
+            getCurrentDirectory: () => path.dirname(configPath), // Crucial for baseUrl resolution
+            getDirectories: ts.sys.getDirectories,
+            realpath: ts.sys.realpath,
+            useCaseSensitiveFileNames: ts.sys.useCaseSensitiveFileNames,
+        };
+
+        const resolved = ts.resolveModuleName(
+            moduleSpecifier,
+            hostFileUri.fragment,
+            parsedCommandLine.options,
+            moduleResolutionHost
+        );
+
+        if (resolved.resolvedModule === undefined) {
+            throw Error(`Failed to resolve uri: ${moduleSpecifier}`); 
+        }
+
+        return vscode.Uri.file(resolved.resolvedModule.resolvedFileName);
+    }
+}
+
+export function uriToBuild(uri: vscode.Uri): [string, vscode.Uri] | undefined {
+    const configPath = ts.findConfigFile(uri.fsPath, ts.sys.fileExists, vscode.workspace.getConfiguration('bazel-import').buildFile);
+    if (!configPath) {
+        return undefined;
+    }
+    const rawTarget = filePathToTargetPath(configPath);
+    const index = rawTarget?.indexOf(vscode.workspace.getConfiguration('bazel-import').buildFile) ?? 1;
+
+    const target = rawTarget?.slice(0, index - 1); 
+    const targetUri = vscode.Uri.file(configPath); 
+    if (target === undefined) {
+        return target;
+    }
+
+    return [target, targetUri]; 
+}
--- src/removedeps.ts
+++ src/removedeps.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { uriToBuildTarget } from './targettools';
 import { uriToContainingUri } from './uritools';
 import { urisFromTextChanges } from './importparse';
+import { uriToBuild, resolveModuleToUri } from './filepathtools';
 
 
 const MIN_LENGTH = 7;
@@ -41,20 +42,12 @@ export const getDeletionTargets = (oldText: string, event: vscode.TextDocumentCh
     return importDeletions; 
 };
 
-const config = new Map<string, string>(); 
-config.set("@lucid", "cake/app/webroot/ts");
-config.set("@scala", "bazel-out/k8-fastbuild/bin/src/jvm/com/lucidchart");
-config.set("lucid-extension-sdk", "extensibility/lucid-extension-sdk"); 
-
-export const getBuildTargetFromFP = async (filePath: string, currentDir: vscode.Uri): Promise<[string, vscode.Uri]> => {
-    for (const [key, value] of config) {
-        if (filePath.startsWith(key)) {
-            filePath = filePath.replace(key, value); 
-            break; 
-        }
-    }
+export const getBuildTargetFromFP = (filePath: string, currentFile: vscode.Uri): [string, vscode.Uri] => {
+    let fileUri: vscode.Uri | undefined;
+    const currentDir = uriToContainingUri(currentFile); 
     if (filePath.startsWith('./')) {
         filePath = currentDir.fsPath + filePath.slice(1) + '.ts'; 
+        fileUri = vscode.Uri.file(filePath); 
     }
     else if (filePath.startsWith('../')) {
         let folderPath = currentDir.fsPath;
@@ -63,16 +56,21 @@ export const getBuildTargetFromFP = async (filePath: string, currentDir: vscode.
             filePath = filePath.slice(3);  
         } 
         filePath = folderPath + '/' + filePath + '.ts'; 
+        fileUri = vscode.Uri.file(filePath); 
     }
     else {
-        const root = vscode.workspace.workspaceFolders![0].uri.fsPath;
-        filePath = root + '/' + filePath + '.ts';
+        fileUri = resolveModuleToUri(currentFile, filePath);
     }
-    
-    const sourceUri: vscode.Uri = vscode.Uri.file(filePath); 
 
-    const target = await uriToBuildTarget(uriToContainingUri(sourceUri));
-    return target!; 
+    if (!fileUri) {
+        throw new Error("Unexpected lack of uri"); 
+    }
+    const target = uriToBuild(fileUri);
+    if (!target) {
+        throw new Error("Unexpected undefined target");
+    }
+    return target;
+        // return (await uriToBuildTarget(uriToContainingUri(fileUri)))!;  
 };
 
 const getImportsFromFile = (fileText: string): RegExpMatchArray[] => {
@@ -89,14 +87,16 @@ export const getBuildTargetsFromFile = async (fileUri: vscode.Uri) => {
 
     const importPositions = importMatches.map(match => file!.positionAt(match.index! + 6)) as vscode.Position[];
     const fileUris = await urisFromPositions(importPositions, fileUri); 
-    const buildUris = [];
-    for (fileUri of fileUris) {
-        const thing = await uriToBuildTarget(uriToContainingUri(fileUri)); // TODO: rename
-        buildUris.push(thing);
-    }
-    return buildUris;
+    const targets = fileUris.map(fileUri => { // 7.49 s (with ts fully loaded)
+        return uriToBuild(fileUri);
+    });
+    // const targets = await Promise.all(fileUris.map(fileUri => { // 6.2s (with ts fully loaded)
+    //     return uriToBuildTarget(uriToContainingUri(fileUri));
+    // }));
+    return targets;
 };
 
 const urisFromPositions = async (positions: vscode.Position[], docUri: vscode.Uri) => {
     return urisFromTextChanges(positions, docUri); 
 };
+
